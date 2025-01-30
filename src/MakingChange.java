2de89404c600f529d3913d68b2fe551526be7ccd
agastyabrahmbhatt <agastyabrahmbhatt@menloschool.org>
2025-01-29 20:32:07 -0800

    code

--- src/MakingChange.java
+++ src/MakingChange.java
@@ -69,42 +69,4 @@ public class MakingChange {
         }
         multiplier_qty(coins,currentTarget,currentIterator,partialSum,0);
     }
-    /*
-    Returns 1, or True, if coin is a factor.
-    Below is one way by using a single coin.
-     */
-    public static long countOne(int target, int coin){
-        int ret = 0;
-        if((target % coin) == 0){
-            ret = 1;
-        }
-        return ret;
-    }
-
-    public static long countTwo(int target, int firstCoin, int secondCoin){
-        int ret = 0;
-        for(int i = 0; i < 2500; i++){
-            for(int j = 0; j < 2500; j++){
-                if((i * firstCoin) + (j * secondCoin) == target){
-                    ret++;
-                }
-            }
-        }
-        return ret;
-    }
-
-    public static long countThree(int target, int firstCoin, int secondCoin, int thirdCoin){
-        int ret = 0;
-        for(int i = 0; i < 2500; i++){
-            for(int j = 0; j < 2500; j++){
-                for(int k = 0; k < 2500; k++){
-                    if((i * firstCoin) + (j * secondCoin) + (k * thirdCoin) == target){
-                        ret++;
-                    }
-                }
-            }
-        }
-        return ret;
-    }
-
 }
